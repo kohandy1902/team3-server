bf0d12821ec04e9f41c410e81a83869fa0af6606
numnum97 <numnum97@naver.com>
2021-11-28 11:08:46 +0900

    ktlint format

--- src/test/kotlin/waffle/team3/wafflestagram/WafflestagramApplicationTests.kt
+++ src/test/kotlin/waffle/team3/wafflestagram/WafflestagramApplicationTests.kt
@@ -1,8 +1,6 @@
 package waffle.team3.wafflestagram
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class WafflestagramApplicationTests {
-}
+class WafflestagramApplicationTests
