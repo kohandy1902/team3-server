a7f666ea40c32782773689102cebcc5a6ce9ffc5
numnum97 <numnum97@naver.com>
2021-11-28 23:32:38 +0900

    Change ping api

--- src/main/kotlin/waffle/team3/wafflestagram/PingController.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/PingController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/")
 class PingController {
 
-    @GetMapping("ping")
+    @GetMapping("ping/")
     fun ping(): String {
         return "pong"
     }
