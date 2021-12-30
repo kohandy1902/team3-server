672475b291b4e74af0c38374a52b7ab7da24a782
Donghyun Koh <andy1902@snu.ac.kr>
2021-12-31 02:53:40 +0900

    Declare FetchType

--- src/main/kotlin/waffle/team3/wafflestagram/domain/User/model/User.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/domain/User/model/User.kt
@@ -5,6 +5,7 @@ import waffle.team3.wafflestagram.domain.Photo.model.Photo
 import waffle.team3.wafflestagram.domain.model.BaseTimeEntity
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.OneToMany
 import javax.persistence.OneToOne
 import javax.persistence.Table
@@ -16,11 +17,11 @@ class User(
     val email: String,
     val password: String? = null,
     var public: Boolean = true,
-    @OneToMany
+    @OneToMany(fetch = FetchType.LAZY)
     var follower: MutableList<User> = mutableListOf(),
-    @OneToMany
+    @OneToMany(fetch = FetchType.LAZY)
     var following: MutableList<User> = mutableListOf(),
-    @OneToMany
+    @OneToMany(fetch = FetchType.LAZY)
     var waitingFollower: MutableList<User> = mutableListOf(),
     var name: String? = null,
     var nickname: String? = null,
