9bbf5d80db99bd3a605cbc00229d98ee763086cb
Donghyun Koh <andy1902@snu.ac.kr>
2022-01-01 03:19:18 +0900

    Add New User Model

---
+++ src/main/kotlin/waffle/team3/wafflestagram/domain/User/model/FollowerUser.kt
@@ -0,0 +1,15 @@
+package waffle.team3.wafflestagram.domain.User.model
+
+import waffle.team3.wafflestagram.domain.model.BaseTimeEntity
+import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "follower_users")
+class FollowerUser (
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: User,
+) : BaseTimeEntity()
---
+++ src/main/kotlin/waffle/team3/wafflestagram/domain/User/model/WaitingFollowerUser.kt
@@ -0,0 +1,15 @@
+package waffle.team3.wafflestagram.domain.User.model
+
+import waffle.team3.wafflestagram.domain.model.BaseTimeEntity
+import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+
+@Entity
+@Table(name = "waiting_follower_users")
+class WaitingFollowerUser (
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: User,
+) : BaseTimeEntity()
