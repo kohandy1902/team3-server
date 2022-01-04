d9742b137d70c673bae608ff9c26d928c49e945d
numnum97 <numnum97@naver.com>
2022-01-04 15:13:33 +0900

    Fix: lint conflict

--- src/main/kotlin/waffle/team3/wafflestagram/domain/Tag/model/Tag.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/domain/Tag/model/Tag.kt
@@ -1,9 +1,21 @@
 package waffle.team3.wafflestagram.domain.Tag.model
 
+import waffle.team3.wafflestagram.domain.Feed.model.Feed
 import waffle.team3.wafflestagram.domain.model.BaseTimeEntity
+import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 import javax.persistence.Table
 
 @Entity
 @Table(name = "tags")
-class Tag() : BaseTimeEntity()
+class Tag(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", referencedColumnName = "id")
+    val feed: Feed,
+
+    @Column
+    var content: String
+) : BaseTimeEntity()
