091cbef73506c11168ecb10e0c509b5838fdbbdb
numnum97 <numnum97@naver.com>
2022-01-04 18:42:52 +0900

    Fix: update CD, health_check

--- scripts/bash/health_check.sh
+++ scripts/bash/health_check.sh
@@ -18,12 +18,14 @@ echo "> Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
 for RETRY_COUNT in {1..10}
 do
     echo "> #${RETRY_COUNT} trying..."
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${TARGET_PORT}/ping/)
 
-    if [ ${RESPONSE_CODE} -eq 200 ]; then
+    RESPONSE=$(curl -s http://127.0.0.1:"${TARGET_PORT}"/ping/)
+    VALID=$(echo "${RESPONSE}" | grep "pong" | wc -l)
+
+    if [ "${VALID}" -ge 1 ]; then
         echo "> New WAS successfully running"
         exit 0
-    elif [ ${RETRY_COUNT} -eq 10 ]; then
+    elif [ "${RETRY_COUNT}" -eq 10 ]; then
         echo "> Health check failed."
         exit 1
     fi
