529c5f8b771d116a878c92be74fbae8d950f68b7
numnum97 <numnum97@naver.com>
2022-01-04 14:35:49 +0900

    Fix: update FeedDto

--- src/main/kotlin/waffle/team3/wafflestagram/domain/Feed/model/Feed.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/domain/Feed/model/Feed.kt
@@ -3,6 +3,7 @@ package waffle.team3.wafflestagram.domain.Feed.model
 import waffle.team3.wafflestagram.domain.Comment.model.Comment
 import waffle.team3.wafflestagram.domain.User.model.User
 import waffle.team3.wafflestagram.domain.model.BaseTimeEntity
+import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.FetchType
@@ -24,6 +25,6 @@ class Feed(
     val user: User,
 
     @Column(name = "comments")
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "feed")
+    @OneToMany(cascade = [CascadeType.ALL], mappedBy = "feed")
     var comments: MutableList<Comment> = mutableListOf(),
 ) : BaseTimeEntity()
--- src/main/kotlin/waffle/team3/wafflestagram/domain/User/model/User.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/domain/User/model/User.kt
@@ -18,18 +18,25 @@ class User(
     val email: String,
     val password: String? = null,
     var public: Boolean = true,
-    @OneToMany(fetch = FetchType.EAGER, cascade = arrayOf(CascadeType.ALL), orphanRemoval = true)
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL], orphanRemoval = true)
     var follower: MutableSet<FollowerUser> = mutableSetOf(),
-    @OneToMany(fetch = FetchType.EAGER, cascade = arrayOf(CascadeType.ALL), orphanRemoval = true)
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL], orphanRemoval = true)
     var following: MutableSet<FollowingUser> = mutableSetOf(),
-    @OneToMany(fetch = FetchType.EAGER, cascade = arrayOf(CascadeType.ALL), orphanRemoval = true)
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = [CascadeType.ALL], orphanRemoval = true)
     var waitingFollower: MutableSet<WaitingFollowerUser> = mutableSetOf(),
+
     var name: String? = null,
     var nickname: String? = null,
     var website: String? = null,
     var bio: String? = null,
-    @OneToOne
-    var profilePhoto: Photo? = null,
-    @OneToMany(mappedBy = "user")
+
+    //  photo 의 경우 s3의 key 로 저장될 거 같습니다!
+    // @OneToOne(mappedBy = "user")
+    // var profilePhoto: Photo? = null,
+
+    @OneToMany(cascade = [CascadeType.ALL], mappedBy = "user")
     var feeds: MutableList<Feed> = mutableListOf(),
 ) : BaseTimeEntity()
