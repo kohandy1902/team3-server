b539614ce7c42cbcc594b97663eae03619e60dda
Donghyun Koh <andy1902@snu.ac.kr>
2022-01-06 20:13:33 +0900

    Fix ReplyController

--- src/main/kotlin/waffle/team3/wafflestagram/domain/Reply/api/ReplyController.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/domain/Reply/api/ReplyController.kt
@@ -5,6 +5,7 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -22,25 +23,25 @@ import javax.validation.Valid
 class ReplyController(
     private val replyService: ReplyService,
 ) {
-    @PostMapping("/")
+    @PostMapping("/{comment_id}/")
     fun createReply(
         @Valid @RequestBody createRequest: ReplyDto.CreateRequest,
         @CurrentUser user: User,
-        @RequestParam("commentId") commentId: Long
+        @PathVariable("comment_id") commentId: Long
     ): ResponseEntity<ReplyDto.Response> {
         val newReply = replyService.create(createRequest, user, commentId)
         return ResponseEntity.ok().body(ReplyDto.Response(newReply))
     }
 
-    @GetMapping("/")
-    fun getReply(@RequestParam("id") id: Long): ResponseEntity<ReplyDto.Response> {
-        val reply = replyService.get(id)
+    @GetMapping("/{reply_id}/")
+    fun getReply(@PathVariable("reply_id") replyId: Long): ResponseEntity<ReplyDto.Response> {
+        val reply = replyService.get(replyId)
         return ResponseEntity.ok().body(ReplyDto.Response(reply))
     }
 
-    @GetMapping("/list/")
+    @GetMapping("/list/{comment_id}/")
     fun getReplyList(
-        @RequestParam("commentId") commentId: Long,
+        @PathVariable("comment_id") commentId: Long,
         @RequestParam(value = "offset", defaultValue = "0") offset: Int,
         @RequestParam(value = "number", defaultValue = "30") limit: Int,
     ): ResponseEntity<Page<ReplyDto.Response>> {
@@ -48,17 +49,17 @@ class ReplyController(
         return ResponseEntity.ok().body(replyList.map { ReplyDto.Response(it) })
     }
 
-    @PutMapping("/")
+    @PutMapping("/{reply_id}/")
     fun updateReply(
         @Valid @RequestBody updateRequest: ReplyDto.UpdateRequest,
-        @RequestParam("id") id: Long
+        @RequestParam("reply_id") replyId: Long
     ): ResponseEntity<ReplyDto.Response> {
-        val reply = replyService.update(updateRequest, id)
+        val reply = replyService.update(updateRequest, replyId)
         return ResponseEntity.ok().body(ReplyDto.Response(reply))
     }
 
-    @DeleteMapping("/")
-    fun deleteReply(@RequestParam("id") id: Long) {
-        replyService.delete(id)
+    @DeleteMapping("/{reply_id}/")
+    fun deleteReply(@RequestParam("reply_id") replyId: Long) {
+        replyService.delete(replyId)
     }
 }
