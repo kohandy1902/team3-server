cb12d8fb336baab68e37bb0a31f61e76213722a0
numnum97 <numnum97@naver.com>
2022-01-07 20:19:34 +0900

    Implement ktlint

--- src/main/kotlin/waffle/team3/wafflestagram/domain/Feed/api/FeedController.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/domain/Feed/api/FeedController.kt
@@ -1,7 +1,6 @@
 package waffle.team3.wafflestagram.domain.Feed.api
 
 import org.springframework.data.domain.Page
-import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
