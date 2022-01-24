ca39951576726f5ba221a54dcc6500fa74230e72
Donghyun Koh <andy1902@snu.ac.kr>
2022-01-25 04:41:37 +0900

    Specify logic

--- src/main/kotlin/waffle/team3/wafflestagram/global/oauth/api/OauthController.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/global/oauth/api/OauthController.kt
@@ -5,13 +5,13 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import waffle.team3.wafflestagram.domain.User.dto.UserDto
-import waffle.team3.wafflestagram.domain.User.exception.UserException
 import waffle.team3.wafflestagram.domain.User.service.UserService
 import waffle.team3.wafflestagram.global.auth.JwtTokenProvider
 import waffle.team3.wafflestagram.global.oauth.exception.AccessTokenException
@@ -46,8 +46,11 @@ class OauthController(
         if (result != null) {
             val payload = result.payload
             if (userService.isAlreadyExists(payload.email))
-                throw UserException("this email already exists")
-            return ResponseEntity.ok()
+                return ResponseEntity.ok()
+                    .header("Authentication", jwtTokenProvider.generateToken(payload.email))
+                    .body(UserDto.Response(userService.getUserByEmail(payload.email)))
+
+            return ResponseEntity.status(HttpStatus.CREATED)
                 .header("Authentication", jwtTokenProvider.generateToken(payload.email))
                 .body(UserDto.Response(userService.getUserByEmail(payload.email)))
         } else {
