b2689cd0804a5aa2156e39d2d8074fa04d397b11
Donghyun Koh <andy1902@snu.ac.kr>
2022-01-27 22:02:44 +0900

    Modify UserPrincipal

--- src/main/kotlin/waffle/team3/wafflestagram/global/auth/model/UserPrincipalDetailService.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/global/auth/model/UserPrincipalDetailService.kt
@@ -4,12 +4,20 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
+import waffle.team3.wafflestagram.domain.User.model.SignupType
 import waffle.team3.wafflestagram.domain.User.repository.UserRepository
 
 @Service
 class UserPrincipalDetailService(private val userRepository: UserRepository) : UserDetailsService {
     override fun loadUserByUsername(s: String): UserDetails {
-        val user = userRepository.findByEmail(s) ?: throw UsernameNotFoundException("User with email '%s' not found")
+        val email = s.substring(0, s.length - 2)
+        val signupTypeInt = s[s.length - 1] - '0'
+        var signupType: SignupType = SignupType.APP
+        for (type in SignupType.values()) {
+            if (type.ordinal == signupTypeInt) signupType = type
+        }
+        val user = userRepository.findByEmailAndSignupType(email, signupType)
+            ?: throw UsernameNotFoundException("User with $email and $signupType not found")
         return UserPrincipal(user)
     }
 }
