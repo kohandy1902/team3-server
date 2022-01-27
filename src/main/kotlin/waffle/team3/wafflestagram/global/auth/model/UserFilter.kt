0e592258e37dfa5d26c7fb60f285c65a13c0c369
Donghyun Koh <andy1902@snu.ac.kr>
2022-01-27 22:52:48 +0900

    Add Function in UserFilter

--- src/main/kotlin/waffle/team3/wafflestagram/global/auth/model/UserFilter.kt
+++ src/main/kotlin/waffle/team3/wafflestagram/global/auth/model/UserFilter.kt
@@ -5,4 +5,19 @@ import waffle.team3.wafflestagram.domain.User.model.SignupType
 class UserFilter(
     val email: String,
     val signupType: SignupType,
-)
+) {
+    override fun toString(): String {
+        return email + " " + signupType.ordinal
+    }
+
+    companion object {
+        fun parseUserFilter(s: String): UserFilter? {
+            val email = s.substring(0, s.length - 2)
+            val signupTypeInt = s[s.length - 1] - '0'
+            for (type in SignupType.values()) {
+                if (type.ordinal == signupTypeInt) return UserFilter(email, type)
+            }
+            return null
+        }
+    }
+}
